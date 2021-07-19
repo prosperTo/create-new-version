f31e36ef7f08b3d25ffb2f6e2c9ae9639eb29c20
Tomas Coeli <tcoeli@gigadata.net>
2021-07-19 12:39:09 -0300

    Resolve Bug fix

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ const start = async () => {
 
   let finalVersion
 
-  const tagsVersion = arrayFilter[arrayFilter.length - 1].ref.split('/')[2]
+  
   const versionTag = JSON.parse(params).versioning
 
   if(arrayFilter.length > 1 ) {
+    const tagsVersion = arrayFilter[arrayFilter.length - 1].ref.split('/')[2]
 
     const higher = tagsVersion.split('-')[0].split('.')[0].substring(1)
     const minium = tagsVersion.split('-')[0].split('.')[1]
